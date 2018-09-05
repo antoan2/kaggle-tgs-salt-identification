7205bb7f79162cfe1f4d4b626aaba420c14b6c5b
antoan2 <antoine.biard.10@gmail.com>
2018-09-05 10:27:16 +0200

    Clean up dataset loader

--- learning-tool/src/dataset/tgs_transforms.py
+++ learning-tool/src/dataset/tgs_transforms.py
@@ -10,48 +10,35 @@ class RandomHorizontalFlip(object):
     def __call__(self, sample):
         if random.random() < 0.5:
             image = sample['image']
-            image_name = sample['image_name']
             mask = sample['mask']
 
-            image = np.flip(image, axis=0).copy()
-            mask = np.flip(mask, axis=0).copy()
-
-            return {'image_name': image_name, 'image': image, 'mask': mask}
-        else:
-            return sample
+            sample['image'] = np.flip(image, axis=0).copy()
+            sample['mask'] = np.flip(mask, axis=0).copy()
+        return sample
 
 
 class RandomVerticalFlip(object):
     def __call__(self, sample):
         if random.random() < 0.5:
             image = sample['image']
-            image_name = sample['image_name']
             mask = sample['mask']
 
-            image = np.flip(image, axis=1).copy()
-            mask = np.flip(mask, axis=1).copy()
+            sample['image'] = np.flip(image, axis=1).copy()
+            sample['mask'] = np.flip(mask, axis=1).copy()
+        return sample
 
-            return {'image_name': image_name, 'image': image, 'mask': mask}
-        else:
-            return sample
 
 class RefractBorders(object):
-
     def __call__(self, sample):
         image, mask = sample['image'], sample['mask']
-        image_name = sample['image_name']
 
         image = skimage.util.pad(image, ((14, 13), (14, 13)), 'reflect')
+        sample['image'] = image
 
-        if mask is None:
-            return {'image_name': image_name, 'image': image, 'mask': None}
-        else:
+        if mask is not None:
             mask = skimage.util.pad(mask, ((14, 13), (14, 13)), 'reflect')
-            return {
-                'image_name': image_name,
-                'image': image,
-                'mask': mask
-            }
+            sample['mask'] = mask
+        return sample
 
 
 class ToTensor(object):
@@ -61,17 +48,13 @@ class ToTensor(object):
         image, mask = sample['image'], sample['mask']
         image_name = sample['image_name']
 
-        # image /= 255.
-        image -= 125
+        image = image - 125
         image = image[np.newaxis, ...]
-        # swap color axis because
-        # numpy image: H x W x C
-        # torch image: C X H X W
-        if mask is None:
-            return {'image_name': image_name, 'image': torch.from_numpy(image).float()}
-        else:
-            return {
-                'image_name': image_name,
-                'image': torch.from_numpy(image).float(),
-                'mask': torch.from_numpy(mask).float()
-            }
+        image = torch.from_numpy(image).float()
+        sample['image'] = image
+
+        if mask is not None:
+            mask = torch.from_numpy(mask).float()
+            sample['mask'] = mask
+
+        return sample
